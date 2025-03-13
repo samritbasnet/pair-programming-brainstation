c2ea3e2387898bcaaf0ef6b7d4f5d485d5f0459e
Samrit Basnet <samritbasnet70@gmail.com>
2025-03-13 12:21:29 -0400

    homepage recipie styling

--- client/recipie/src/App.jsx
+++ client/recipie/src/App.jsx
@@ -2,15 +2,11 @@ import { useState } from 'react';
 import RecipieCard from './components/RecipieCard/RecipieCard';
 import './App.scss';
 
-
 function App() {
-
-
   return (
     <>
       <div>
-        <h1>Recipie App</h1>
-        <RecipieCard/>
+        <RecipieCard />
       </div>
     </>
   );
